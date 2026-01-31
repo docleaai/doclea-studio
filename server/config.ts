711d1281acb421f769f81304f18ed4b9a73b4ada
dusan-nikcevic <168220721+dusan-nikcevic@users.noreply.github.com>
2026-01-31 22:56:28 +0100

    stuff

--- server/config.ts
+++ server/config.ts
@@ -1,10 +1,9 @@
-import { homedir } from "node:os";
 import { join, resolve } from "node:path";
 
 export const config = {
-  docleaPath: process.env.DOCLEA_PATH || join(process.cwd(), ".doclea"),
-  port: Number.parseInt(process.env.PORT || "5000"),
-  host: "127.0.0.1", // Localhost only for security
+	docleaPath: process.env.DOCLEA_PATH || join(process.cwd(), ".doclea"),
+	port: Number.parseInt(process.env.PORT || "5000", 10),
+	host: "127.0.0.1", // Localhost only for security
 };
 
 export const LOCAL_DB = resolve(config.docleaPath, "local.db");
